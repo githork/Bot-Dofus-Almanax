34f007731cbeb5187f2015c3918ed899f28467cf
Francisco Inostroza Núñez <33992049+Javadod@users.noreply.github.com>
2021-02-11 18:45:29 -0300

    Update otros.functions.js
    Algunos servicios toman de diferente forma el módulo time, por lo que se ha modificado a 12 y 24 hrs.

--- src/functions/otros.functions.js
+++ src/functions/otros.functions.js
@@ -15,9 +15,24 @@ function aviso_con_intervalo (intervalo_ms, client, id_canal) { // ms = 1000
     let aviso = setInterval(function () {
         let hora_francia = hora_actual()[1].hora.toString()
         let lista = hora_francia.split(':')
-        let h = lista[0], m = lista[1], s = lista[2]
-        hora_francia == '12:00:00' || hora_francia == '00:00:00' ? ver_ofrenda_fecha(null, "", client, id_canal) : false
-        h == '23' ? ((m == '30' || m == '45' || m == '50') && s == '00' ? ver_ofrenda_fecha(null, "", client, id_canal) : false) : false
+        let h = lista[0], m = lista[1], s, medio
+        try {
+            if (lista[2].split(' ').length >= 2) {
+                s = lista[2].split(' ')[0]
+                medio = lista[2].split(' ')[1]
+            } else {
+                s = lista[2]
+                medio = null
+            }
+        } catch (e) {
+            s = lista[2]
+            medio = null
+        }
+        h == '12' || h == '06' || h == '00' ? ( m == '00' && s == '00' ? ver_ofrenda_fecha(null, "", client, id_canal) : false) : false
+        if (medio == 'PM')
+            h == '12' ? ((m == '30' || m == '45' || m == '50') && s == '00' ? ver_ofrenda_fecha(null, "", client, id_canal) : false) : false
+        else
+            h == '23' ? ((m == '30' || m == '45' || m == '50') && s == '00' ? ver_ofrenda_fecha(null, "", client, id_canal) : false) : false
     }, intervalo_ms)
     return aviso
 }
